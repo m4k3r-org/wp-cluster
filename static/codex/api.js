fe369610180ed455073712b5650638f631266e78
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-10-01 12:23:22 -0400

    WIP

---
+++ static/codex/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "API",
+        "Debug",
+        "Flawless",
+        "Loader",
+        "Log",
+        "Media",
+        "Security",
+        "Settings",
+        "Theme",
+        "Utility",
+        "Varnish",
+        "Views"
+    ],
+    "modules": [
+        "Varnish"
+    ],
+    "allModules": [
+        {
+            "displayName": "Varnish",
+            "name": "Varnish",
+            "description": "API Access Controller"
+        }
+    ]
+} };
+});
