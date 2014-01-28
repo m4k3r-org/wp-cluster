24f47a9b7058faf2d929c49771b0070b7fac6215
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-01-28 02:29:44 -0500

    wip

---
+++ lib/class-utility.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Festival Utility
+ *
+ * @version 0.1.0
+ * @author Usability Dynamics
+ * @namespace UsabilityDynamics
+ */
+namespace UsabilityDynamics\Festival {
+
+  /**
+   * Festival Utility
+   *
+   * @author Usability Dynamics
+   */
+  class Utility extends \UsabilityDynamics\Utility {
+
+
+
+  }
+
+}
