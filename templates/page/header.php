9654dc7d6deb3aa1468c3d704db9d28ce4e956f8
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-01-28 09:34:17 -0500

    Automated commit.

--- templates/page/header.php
+++ templates/page/header.php
@@ -16,29 +16,18 @@ $cc[] = $festival->get_query_template();
 $cc[] = ( is_home() || is_front_page() ) ? 'front-page' : 'inner-page';
  
 ?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
+<!--[if IE 7]> <html class="ie ie7" <?php language_attributes(); ?> <![endif]-->
+<!--[if IE 8]> <html class="ie ie8" <?php language_attributes(); ?> <![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]> <html <?php language_attributes(); ?> <!--<![endif]-->
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon.png">
     <?php wp_head(); ?>
   </head>
   
