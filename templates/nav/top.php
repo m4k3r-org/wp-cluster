0391792de28cb0800cb4a0070da28ad1ea3fe6a3
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-06 10:49:50 -0500

    Automated commit.

--- templates/nav/top.php
+++ templates/nav/top.php
@@ -8,8 +8,9 @@
  */
 ?>
 <!-- Navigation bar starts -->
-<div class="navbar navbar-inverse navbar-top navbar-top-home navbar-primary" role="navigation">
+<nav class="navbar navbar-inverse navbar-top navbar-top-home navbar-primary" role="navigation">
   <div class="container">
+
     <div class="social-wrap">
       <a class="btn btn-default" role="button" href="#" ><?php _e( 'Buy Tickets', wp_festival( 'domain' ) ); ?></a>
       <div class="no-sticky social-flex">
@@ -20,6 +21,7 @@
         <div class="social-collapse collapse"><?php get_template_part( 'templates/aside/social', get_post_type() ); ?></div>
       </div>
     </div>
+
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?php _e( 'Toggle navigation', wp_festival( 'domain' ) ); ?></span>
@@ -31,9 +33,11 @@
         <a class="logo sticky" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/temp/small-logo.png" /></a>
       </span>
     </div>
+
     <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
       <?php echo wp_festival()->nav( 'primary', 2 ); ?>
     </nav>
+
   </div>
-</div>
+</nav>
 <!-- Navigation bar ends -->
