50e0d0ff39327c7ec718a67eb4f7c871656ba9e4
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-06 04:20:14 -0500

    wip

---
+++ scripts/src/app.main.js
@@ -0,0 +1,36 @@
+/**
+ * Main Application Scripts
+ *
+ * @example
+ *
+ *      // Some Locale String.
+ *      require( 'site.locale' ).someWord
+ *
+ *      // AJAX URL.
+ *      require( 'site.model' ).ajax
+ *
+ */
+define( [ 'site.locale', 'site.model', 'jquery', 'skrollr', 'twitter.bootstrap' ], function( locale, model, jQuery ) {
+  console.log( 'app', 'loaded', require( 'site.model' ).domain );
+
+  window.skrollr.init( {
+    forceHeight: false
+  });
+
+
+  // Sticky elements implementation
+  require( [ 'sticky' ], function() {
+    var st = 0;
+
+    if( jQuery( '#wpadminbar' ).length > 0 ) {
+      st = jQuery( '#wpadminbar' ).height();
+    }
+
+    jQuery( ".navbar-top-home" ).sticky( {
+      topSpacing: st
+    });
+
+  });
+
+});
+
