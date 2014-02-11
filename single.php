96c1f86bead2b39cf24ba5cfd4b55aa0b85fa55a
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-02-11 17:43:50 +0200

    single page

--- single.php
+++ single.php
@@ -17,6 +17,7 @@ get_template_part( 'templates/header', get_post_type() );
         <?php while( have_posts() ) : the_post(); ?>
           <?php get_template_part( 'templates/article/content', get_post_type() ); ?>
         <?php endwhile; ?>
+        <?php get_template_part( 'templates/article/author', get_post_type() ); ?>
         <?php comments_template( '/templates/article/comments.php' ); ?>
       </section>
     </div>
