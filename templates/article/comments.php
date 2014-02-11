c0e7533e2c1abc3b612186bfdcfd2668ae48d602
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-02-11 19:32:21 +0200

    comments

--- templates/article/comments.php
+++ templates/article/comments.php
@@ -21,17 +21,19 @@ if ( post_password_required() )
 
 ?>
 <?php if( comments_open( get_the_ID() ) ) : ?>
-  <?php if ( have_comments() ) : ?>
-    <!-- Comment section -->
-    <div id="comments" class="comments" id="comments">
-      <div class="title"><h5><?php comments_number( 'No Comments', 'One Comment', '% Comments' ); ?></h5></div>
-      <ul class="comment-list">
-        <?php wp_list_comments( array( 'style' => 'ul', 'short_ping' => true, 'avatar_size' => 64 ) ); ?>
-      </ul>
-    </div>
-  <?php endif; // have_comments() ?>
-  <!-- Comment posting -->
-  <div class="respond well">
-    <?php comment_form( array( 'comment_notes_after' => '' ) ); ?>
-  </div>
+  <article class="comments">
+    <?php if ( have_comments() ) : ?>
+      <!-- Comment section -->
+      <section class="comments-list">
+        <div class="title"><h5><?php comments_number( 'No Comments', 'One Comment', '% Comments' ); ?></h5></div>
+        <ul>
+          <?php wp_list_comments( array( 'style' => 'ul', 'short_ping' => true, 'avatar_size' => 64 ) ); ?>
+        </ul>
+      </section>
+    <?php endif; // have_comments() ?>
+    <!-- Comment posting -->
+    <section class="respond well">
+      <?php comment_form( array( 'comment_notes_after' => '' ) ); ?>
+    </section>
+  </article>
 <?php endif; ?>
