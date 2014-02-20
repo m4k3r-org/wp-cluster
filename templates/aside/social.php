1071873d2e172c4836f59b2ea615847d1df5624d
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-02-20 20:14:57 +0200

    CSS

--- templates/aside/social.php
+++ templates/aside/social.php
@@ -10,7 +10,7 @@
 ?>
 <ul class="social">
   <li><a href="#"><span class="icon-facebook"></span></a></li>
-  <li><a href="#"><span class="icon-youtube"></span></a></li>
+  <li><a href="#"><span class="icon-youtube-play"></span></a></li>
   <li><a href="#"><span class="icon-twitter"></span></a></li>
   <li><a href="#"><span class="icon-google-plus"></span></a></li>
   <li><a href="#"><span class="icon-instagram"></span></a></li>
