7fd48d97eb0964f70c841eea923c082490f6cf2b
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-02-21 16:39:27 +0200

    templates

---
+++ templates/article/listing-post-slider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Slider Post Content
+ *
+ * @author Usability Dynamics
+ * @module festival
+ * @since festival 0.1.0
+ */
+
+?>
+<article class="post-slider">
+  <div class="image">
+    <img class="img-responsive" src="<?php echo wp_festival()->get_image_link_by_post_id( get_the_ID(), array( 'width' => '738', 'height' => '415' ) ); ?>" />
+    <h4 class="category">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      <span class="hr"></span>
+    </h4>
+  </div>
+  <div class="text">
+    <div class="content"><?php the_excerpt(); ?></div>
+    <?php get_template_part( 'templates/aside/share', get_post_type() ); ?>
+  </div>
+</article>
--- templates/aside/post-loop.php
+++ templates/aside/post-loop.php
@@ -9,6 +9,8 @@
 
 global $wp_query;
 
+$type = 'featured';
+
 ?>
 
 <div class="posts-loop-module container">
@@ -20,17 +22,7 @@ global $wp_query;
             <?php wp_festival()->set_excerpt_filter( '25', 'length' ); ?>
             <?php while( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
               <li class="news-item">
-                <div class="image">
-                  <img class="img-responsive" src="<?php echo wp_festival()->get_image_link_by_post_id( get_the_ID(), array( 'width' => '738', 'height' => '415' ) ); ?>" />
-                  <h4 class="category">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    <span class="hr"></span>
-                  </h4>
-                </div>
-                <div class="text">
-                  <div class="content"><?php the_excerpt(); ?></div>
-                  <?php get_template_part( 'templates/aside/share', get_post_type() ); ?>
-                </div>
+                <?php get_template_part( 'templates/article/listing-post', $type ); ?>
               </li>
             <?php endwhile; ?>
             <?php wp_festival()->set_excerpt_filter( false, 'length' ); ?>
