b7940334bbe2d397e823e42a43b7bee324150b1d
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-25 06:23:33 -0500

    Automated commit.

--- lib/local/advanced-cache.php
+++ lib/local/advanced-cache.php
@@ -20,7 +20,7 @@ if (!defined('W3TC_IN_MINIFY')) {
 
     if (!@is_dir(W3TC_DIR) || !file_exists(W3TC_DIR . '/inc/define.php')) {
         if (defined('WP_ADMIN')) { // lets don't show error on front end
-            echo(sprintf('<strong>W3 Total Cache Error:</strong> some files appear to be missing or out of place. Please re-install plugin or remove <strong>%s</strong>.', __FILE__));
+            //trigger_error(sprintf('<strong>W3 Total Cache Error:</strong> some files appear to be missing or out of place. Please re-install plugin or remove <strong>%s</strong>.', __FILE__));
         }
     } else {
         require_once W3TC_DIR . '/inc/define.php';
