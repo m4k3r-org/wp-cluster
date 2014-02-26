ffcc49933de3f09bea4ebffe8ac234637923cf3c
Anton Korotkov <anton.korotkov@usabilitydynamics.com>
2014-02-26 18:53:14 +0200

    hidden URL from comments

--- lib/class-festival.php
+++ lib/class-festival.php
@@ -378,6 +378,21 @@ namespace UsabilityDynamics {
         $this->menufication = \Menufication::getInstance();
       }
 
+      //
+      add_filter('comment_form_default_fields', array( $this, 'url_filtered' ) );
+
+    }
+
+    /**
+     *
+     * @param type $fields
+     * @return type
+     */
+    public function url_filtered( $fields ) {
+      if( isset( $fields['url'] ) ) {
+        unset($fields['url']);
+      }
+      return $fields;
     }
 
     /**
