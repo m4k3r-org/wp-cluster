bd32edcb5d433212aa4a2aa6fde949fc5ded15f3
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-26 12:03:28 -0500

    Automated commit.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(){jQuery("article.container").delay(1e3).fadeIn(2e3,function(){})}),define("app",function(){});
+jQuery(document).ready(function(){jQuery("article.container").delay(1e3).fadeIn(2e3,function(){})});
