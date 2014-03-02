07eb350870ae971a7b2cd22f1c90edb019430976
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-03-02 17:56:51 -0500

    added support for www/apex domain lookup

--- lib/local/sunrise.php
+++ lib/local/sunrise.php
@@ -4,49 +4,46 @@
  *
  * SELECT blog_id FROM corporate_blogs WHERE domain IN ('www.udx.io','udx.io') ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1
  *
- * Going to www.udx.io in browser will end up being here as apex-origin.udx.io given the way CF is configured.
- * To handle www.udx.io -> udx.io redirection extra logic will be nesarry - right now both will work.
+ * Manually going to origin.udx.io will redirect to udx.io while www.origin.udx.io will redirect to www.udx.io.
  *
- * Manually going to apex-origin.udx.io will redirect to udx.io.
- *
- * @version 0.4.1
+ * @version 0.4.2
  */
 
 // Disable caching to avoid errors being cached by CloudFront.
-header( "Cache-Control: no-cache" );
+header( "Cache-Control: no-cache, max-age=0" );
 
 if( !defined( 'SUNRISE_LOADED' ) ) {
   define( 'SUNRISE_LOADED', 1 );
 }
 
 if( defined( 'COOKIE_DOMAIN' ) ) {
-  header( "Status: 500 Not Found" );
+  header( 'HTTP/1.1 500 Internal Server Error' );
   wp_die( '<h1>Network Error</h1><p>The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.</p>' );
 }
 
-if( !count( $wpdb->get_col( "SHOW TABLES" ) ) ) {
-  header( "Status: 500 Not Found" );
+if( !isset( $wpdb ) || !count( $wpdb->get_col( "SHOW TABLES" ) ) ) {
+  header( 'HTTP/1.1 500 Internal Server Error' );
   wp_die( '<h1>Network Error</h1><p>The network database is not setup.</p>' );
 }
 
 $_host = $_SERVER[ 'HTTP_HOST' ];
 
 // Amazon CloudFront gets access.
-if( $_SERVER['HTTP_USER_AGENT'] === 'Amazon CloudFront' ) {
-  $_host = str_replace( 'www-origin.', 'www.', $_host );
-  $_host = str_replace( 'apex-origin.', '', $_host );
+if( $_SERVER[ 'HTTP_USER_AGENT' ] === 'Amazon CloudFront' ) {
+  $_host = str_replace( 'www.origin.', 'www.', $_host );
+  $_host = str_replace( 'origin.', '', $_host );
 }
 
 // Veneer API Proxy Gets gets access.
-if( $_SERVER['HTTP_USER_AGENT'] === 'Veneer' ) {
-  $_host = str_replace( 'www-origin.', 'www.', $_host );
-  $_host = str_replace( 'apex-origin.', '', $_host );
+if( $_SERVER[ 'HTTP_USER_AGENT' ] === 'Veneer' ) {
+  $_host = str_replace( 'www.origin.', 'www.', $_host );
+  $_host = str_replace( 'origin.', '', $_host );
 }
 
 // Uncaught origin request - strip away possible origins and forward to public primary.
-if( strpos( $_host, '-origin.' ) > 0 ) {
-  $_host = str_replace( 'www-origin.', 'www.', $_host );
-  $_host = str_replace( 'apex-origin.', '', $_host );
+if( strpos( $_host, 'origin.' ) > 0 ) {
+  $_host = str_replace( 'www.origin.', 'www.', $_host );
+  $_host = str_replace( 'origin.', '', $_host );
   header( "Location: http://{$_host}{$_SERVER['REQUEST_URI']}" );
   exit();
 }
@@ -55,17 +52,18 @@ if( strpos( $_host, '-origin.' ) > 0 ) {
 $_host = str_replace( array( 'static.', 'assets.', 'media.', 'public.' ), '', $_host );
 
 // Lookup both versions, returning the longer.
-if( ( $nowww = preg_replace( '|^www\.|', '', $_host ) ) != $_host )
+if( ( $nowww = preg_replace( '|^www\.|', '', $_host ) ) != $_host ) {
   $where = $wpdb->prepare( 'domain IN (%s,%s)', $_host, $nowww );
-else
-  $where = $wpdb->prepare( 'domain = %s', $_host );
+} else {
+  $where = $wpdb->prepare( 'domain IN (%s,%s)', $_host, 'www.' . $_host );
+}
 
 // Order by char length in case of multiple results essentially gives the longer domain more prevelance.  
 if( $current_blog = $wpdb->get_row( "SELECT * FROM {$wpdb->blogs} WHERE {$where} ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" ) ) {
 
   // Define globals.
-  $blog_id              = $current_blog->blog_id;
-  $site_id              = $current_blog->site_id;
+  $blog_id = $current_blog->blog_id;
+  $site_id = $current_blog->site_id;
 
   // Add cookie with subdomain support.
   if( !defined( 'COOKIE_DOMAIN' ) ) {
@@ -103,7 +101,8 @@ if( $current_blog = $wpdb->get_row( "SELECT * FROM {$wpdb->blogs} WHERE {$where}
 
 }
 
-header( "Status: 404 Not Found" );
-wp_die( '<h1>Network Error</h1><p>The domain you requested (' . $_host . ') is not available on network. This is nginx.</p>' );
+die($wpdb->last_query);
+header( 'HTTP/1.1 404 Not Found' );
+wp_die( '<h1>Network Error</h1><p>The domain you requested (' . $_host . ') is not available on network.</p>' );
 
 
