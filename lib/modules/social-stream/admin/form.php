f37ab6c7f27a8554310773769bef00ba2f2601ce
Anton Korotkov <anton.korotkov@usabilitydynamics.com>
2014-03-12 12:03:14 +0200

    social stream fix

--- lib/modules/social-stream/admin/form.php
+++ lib/modules/social-stream/admin/form.php
@@ -1,12 +1,13 @@
+
 <p>
   <label for="<?php echo $this->get_field_id( 'wall' ); ?>"><?php _e( 'Use Wall' ) ?></label>
   <select id="<?php echo $this->get_field_id( 'wall' ); ?>" name="<?php echo $this->get_field_name( 'wall' ); ?>" style="width:100%;">
-    <option value="false" <?php selected( 'false', $instance[ 'wall' ], true ); ?>>No</option>
-    <option value="true" <?php selected( 'true', $instance[ 'wall' ], true ); ?>>Yes</option>
+    <option value="false" <?php selected( 'false', $data[ $this->get_field_name( 'wall' ) ], true ); ?>>No</option>
+    <option value="true" <?php selected( 'true', $data[ $this->get_field_name( 'wall' ) ], true ); ?>>Yes</option>
   </select>
 </p>
 
 <p>
   <label for="<?php echo $this->get_field_id( 'rotate_delay' ); ?>"><?php _e( 'Rotate Delay (ms)' ) ?></label>
-  <input type="text" value="<?php echo !empty( $instance[ 'rotate_delay' ] )?$instance[ 'rotate_delay' ]:''; ?>" id="<?php echo $this->get_field_id( 'rotate_delay' ); ?>" name="<?php echo $this->get_field_name( 'rotate_delay' ); ?>" />
+  <input type="text" value="<?php echo !empty( $data[ $this->get_field_name( 'rotate_delay' ) ] )?$data[ $this->get_field_name( 'rotate_delay' ) ]:''; ?>" id="<?php echo $this->get_field_id( 'rotate_delay' ); ?>" name="<?php echo $this->get_field_name( 'rotate_delay' ); ?>" />
 </p>
