a53c0a0f94748b508a2f458f01190e0125fc98a4
Anton Korotkov <anton.korotkov@usabilitydynamics.com>
2014-03-19 18:52:39 +0200

    stream fix

--- scripts/src/socialstream.js
+++ scripts/src/socialstream.js
@@ -71,7 +71,7 @@ define( 'socialstream', [ 'jquery.socialstream', 'jquery.socialstream.wall' ], f
     jQuery(window).load(function(){
       console.debug('stream loaded');
       if ( that.data('moderate') == '1' ) {
-        jQuery('.stream .dcsns-li').prepend('<a href="javascript:;">x</a>');
+        //jQuery('.stream .dcsns-li').prepend('<a href="javascript:;">x</a>');
       }
     });
 
