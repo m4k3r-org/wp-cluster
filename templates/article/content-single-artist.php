9d61664d64c0e2aa4b7769a9ef633926536c6204
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-03-27 20:30:15 +0200

    working on single artist page

--- templates/article/content-single-artist.php
+++ templates/article/content-single-artist.php
@@ -2,15 +2,28 @@
 /**
  * Artist Content template
  */
+
+$artist = wp_festival()->get_post_data( get_the_ID() );
+ 
 ?>
 
-<?php get_template_part('templates/aside/artist-hero'); ?>
+<section class="article-content" data-type="content">
+  <div class="container">
+  
+    <div class="section">
+      <?php get_template_part('templates/aside/artist-hero'); ?>
+    </div>
 
-<!-- Social Stream Module -->
-<?php echo do_shortcode( '[social_stream 
-  youtube_search_for="" 
-  facebook_search_for="" 
-  instagram_search_for=""
-  twitter_search_for=""
-]' ); ?>
-<!-- #Social Stream Module -->
+    <!-- Social Stream Module -->
+    <div class="section">
+      <?php echo do_shortcode( '[social_stream 
+        ' . ( !empty( $artist[ 'usernameYoutube' ] ) ? "youtube_search_for=\"{$artist[ 'usernameYoutube' ]}\"" : '' ) . '
+        ' . ( !empty( $artist[ 'usernameFacebook' ] ) ? "facebook_search_for=\"{$artist[ 'usernameFacebook' ]}\"" : '' ) . '
+        ' . ( !empty( $artist[ 'usernameInstagram' ] ) ? "instagram_search_for=\"!{$artist[ 'usernameInstagram' ]}\"" : '' ) . '
+        ' . ( !empty( $artist[ 'usernameTwitter' ] ) ? "twitter_search_for=\"{$artist[ 'usernameTwitter' ]}\"" : '' ) . '
+      ]' ); ?>
+    </div>
+    <!-- #Social Stream Module -->
+
+  </div>
+</section>
