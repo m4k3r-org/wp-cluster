2683e9f2899af237fc61f59f21432264e07a9070
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-06 00:10:01 -0400

    wip

---
+++ lib/class-bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * @author Andy Potanin <andy.potanin@usabilitydynamics.com>
+ */
+namespace wpCloud\Vertical\EDM {
+
+
+  class Bootstrap {
+
+    function initialize() {
+
+      // add_action( 'network_admin_menu', 'wpCloud\Vertical\EDM::admin_menu', 20 );
+
+    }
+
+  }
+
+}
