435b58eeb3bfe599556e5323a968353c6c29c09a
Anton K <antonkorotkoff@gmail.com>
2014-06-11 14:29:46 +0300

    event taxonomy pages

--- entities/event-taxonomy.php
+++ entities/event-taxonomy.php
@@ -12,95 +12,14 @@ namespace DiscoDonniePresents {
      */
     class EventTaxonomy extends Taxonomy {
 
-      public function __construct($id = false, $taxonomy = false) {
-        parent::__construct($id, $taxonomy);
-
-        $this->load_data();
-      }
-
       /**
        *
        * @var type
        */
-      public $_events;
-
-      /**
-       *
-       * @var type
-       */
-      public $_photos;
-
-      /**
-       *
-       * @var type
-       */
-      public $_videos;
-
-      /**
-       *
-       */
-      public function load_data() {
-
-        $_objects = get_posts(array(
-          'post_type' => 'event',
-          'posts_per_page' => -1,
-          'tax_query' => array(
-            array(
-              'taxonomy' => $this->_term->taxonomy,
-              'field' => 'slug',
-              'terms' => $this->_term->slug
-            )
-          )
-        ));
-
-        if ( !empty( $_objects ) ) {
-
-          foreach( (array)$_objects as $_object ) {
-
-            $this->_events[] = new Event( $_object->ID, false);
-
-          }
-
-        }
-
-        if ( !empty( $this->_events ) ) {
-
-          foreach( $this->_events as $_event ) {
-
-            if ( $_photo = $_event->photo() ) {
-              $this->_photos[] = $_photo;
-            }
-
-            if ( $_video = $_event->video() ) {
-              $this->_videos[] = $_video;
-            }
-
-          }
-
-        }
-
-      }
-
-      /**
-       *
-       */
-      public function events() {
-        return $this->_events;
-      }
-
-      /**
-       *
-       */
-      public function photos() {
-        return $this->_photos;
-      }
-
-      /**
-       *
-       */
-      public function videos() {
-        return $this->_videos;
-      }
+      public $_taxToElasticField = array(
+          'age-limit' => 'age_restriction',
+          'event-type' => 'event_type'
+      );
 
     }
 
