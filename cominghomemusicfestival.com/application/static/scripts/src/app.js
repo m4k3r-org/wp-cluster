1fae3b723a0024054434f48a622e1104f95b966f
George Fekete <george@primalskill.com>
2014-07-14 13:59:39 +0300

    CHMF mocks

---
+++ cominghomemusicfestival.com/application/static/scripts/src/app.js
@@ -0,0 +1,12 @@
+/**
+ * Main entry point for Javascript. Loads several sub-modules and sets up the page.
+ *
+ * @module CHMF
+ * @main CHMF
+ */
+require( [
+  'jquery'
+], function( $ ){
+
+
+} );
