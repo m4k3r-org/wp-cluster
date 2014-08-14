91ad84c7aad43ca7b3e69bf9d71361407fbc34a7
attilaf <attilaf@primalskill.com>
2014-08-14 14:23:12 +0300

    Register countdown contest widget

--- lib/widgets/widget-bootstrap.php
+++ lib/widgets/widget-bootstrap.php
@@ -3,6 +3,7 @@
 namespace WP_Spectacle;
 
 require_once('artist-lineup/artist-lineup.php');
+require_once('contest-countdown/contest-countdown.php');
 
 class Widget_Bootstrap
 {
@@ -25,9 +26,11 @@ class Widget_Bootstrap
       register_widget( 'WP_Spectacle\Widgets\ArtistLineup' );
     });
 
+    wp_register_script('artist-lineup-widget-admin', __DIR__ .'/artist-lineup/static/scripts/artist-lineup-widget-admin.js');
+
     // Init widget scripts and styles
     add_action( 'wp_enqueue_scripts', function (){
-      wp_enqueue_script( 'artist-lineup-widget-admin', __DIR__ .'/artist-lineup/static/scripts/artist-lineup-widget-admin.js', array(), '', true );
+      wp_enqueue_script( 'artist-lineup-widget-admin');
     });
 
     // Add shortcode for widget
@@ -41,6 +44,38 @@ class Widget_Bootstrap
 
       ob_start();
       the_widget( 'WP_Spectacle\Widgets\ArtistLineup', $atts, $args );
+      $output = ob_get_clean();
+
+      return $output;
+
+    });
+
+    return $this;
+  }
+
+  public function init_contest_countdown()
+  {
+    // Init widget
+    add_action( 'widgets_init', function() {
+      register_widget( 'WP_Spectacle\Widgets\ContestCountdown' );
+    });
+
+    // Init widget scripts and styles
+    add_action( 'wp_enqueue_scripts', function (){
+      wp_enqueue_script( 'contest-countdown-widget-admin', __DIR__ .'/contest-countdown/static/scripts/contest-countdown-widget-admin.js', array(), '', true );
+    });
+
+    // Add shortcode for widget
+    add_shortcode( 'widget_contest_countdown', function( $atts ) {
+      // Configure defaults and extract the attributes into variables
+      extract( shortcode_atts( array(
+        'title' => '',
+        'description' => '',
+        'dates' => ''
+      ), $atts ) );
+
+      ob_start();
+      the_widget( 'WP_Spectacle\Widgets\ContestCountdown', $atts, $args );
       $output = ob_get_clean();
 
       return $output;
