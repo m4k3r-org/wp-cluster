226380fb6589926df220d5372f1808250a33872d
Reid Williams <reid.williams@usabilitydynamics.com>
2014-09-01 04:51:59 -0400

    * Merging in 'develop' branch * Updating plugins [ci skip]

--- gruntfile.js
+++ gruntfile.js
@@ -143,6 +143,34 @@ module.exports = function( grunt ) {
       }
     },
 
+    // Clean Directories.
+    clean: {
+      files: [
+        '.environment',
+        '.htaccess',
+        'advanced-cache.php',
+        'db.php',
+        'object-cache.php',
+        'sunrise.php',
+        'vendor/libraries/automattic/wordpress/wp-config.php',
+        'wp-cli.yml'
+      ],
+      symlinks: [
+        '.htaccess',
+        'advanced-cache.php',
+        'db.php',
+        'object-cache.php',
+        'sunrise.php',
+        'vendor/libraries/automattic/wordpress/wp-config.php',
+        'wp-cli.yml'
+      ],
+      junk: [
+        'cgi-bin',
+        'uploads'
+      ],
+      test: []
+    },
+
     // Build Our Less Assets
     less: {
       development: {
@@ -190,6 +218,98 @@ module.exports = function( grunt ) {
             max_line_length: 1000,
             no_mangle: true
           }
+        }
+      }
+    },
+
+    // Symbolic Links.
+    symlink: {
+      standalone: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php'
+        }
+      },
+      network: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php',
+          'sunrise.php': 'vendor/modules/wp-cluster/lib/class-sunrise.php'
+        }
+      },
+      cluster: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php',
+          'db.php': 'vendor/modules/wp-cluster/lib/class-database.php',
+          'sunrise.php': 'vendor/modules/wp-cluster/lib/class-sunrise.php'
+        }
+      },
+      production: {
+        files: {
+          'wp-cli.yml': 'application/static/etc/wp-cli.yml',
+          'advanced-cache.php': 'vendor/modules/wp-veneer/lib/class-advanced-cache.php',
+          'object-cache.php': 'vendor/modules/wp-veneer/lib/class-object-cache.php'
+        }
+      },
+      development: {
+        files: {
+          'wp-cli.yml': 'application/static/etc/wp-cli.yml'
+        }
+      },
+      staging: {},
+      local: {}
+    },
+
+    // Copying files (for Windows)
+    copy: {
+      standalone: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php'
+        }
+      },
+      network: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php',
+          'sunrise.php': 'vendor/modules/wp-cluster/lib/class-sunrise.php'
+        }
+      },
+      cluster: {
+        files: {
+          '.htaccess': 'vendor/modules/wp-veneer/lib/local/.htaccess',
+          'vendor/libraries/automattic/wordpress/wp-config.php': 'vendor/modules/wp-veneer/lib/class-config.php',
+          'db.php': 'vendor/modules/wp-cluster/lib/class-database.php',
+          'sunrise.php': 'vendor/modules/wp-cluster/lib/class-sunrise.php',
+        }
+      },
+      production: {
+        files: {
+          'advanced-cache.php': 'vendor/modules/wp-veneer/lib/class-advanced-cache.php',
+          'object-cache.php': 'vendor/modules/wp-veneer/lib/class-object-cache.php'
+        }
+      },
+      development: {
+        files: {
+          'wp-cli.yml': 'application/static/etc/wp-cli.yml'
+        }
+      },
+      staging: {},
+      local: {}
+    },
+
+    // Shell commands
+    shell: {
+      // This just configures the environment file
+      configure: {
+        options: {
+          stdout: true
+        },
+        command: function( environment ){
+          var cmd = 'echo ' + environment + ' > ./.environment';
+          grunt.log.writeln( 'Running command: ' + cmd );
+          return cmd;
         }
       }
     },
@@ -263,7 +383,14 @@ module.exports = function( grunt ) {
       }
     }
 
-  });
+  } );
+
+  // Pull in some NPM based tasks
+  grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-less' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-yuidoc' );
 
   // Automatically Load Tasks from application/tasks directory
   grunt.task.loadTasks( 'application/tasks' );
