8e0a16b4f1ac2b84a58534eaeaabe00599de4ea3
Reid Williams <reid.williams@usabilitydynamics.com>
2014-09-08 20:17:27 +0000

    * Updating composer, removing MBP

--- vendor/libraries/autoload.php
+++ vendor/libraries/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb900cd3abc87b7c0f6a3175ed1cbbab5::getLoader();
+return ComposerAutoloaderInit1dae88166d6de55b32b1fac2e2fcf47f::getLoader();
