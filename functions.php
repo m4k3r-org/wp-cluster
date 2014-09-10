3f0e9ad495479f18e9455db34f2ae223c655aac1
andypotanin <andy.potanin@usabilitydynamics.com>
2014-09-10 00:07:49 -0400

    [ci skip]

--- functions.php
+++ functions.php
@@ -763,6 +763,8 @@ class hddp extends Flawless_F {
         'post_type' => ''
     ), $args );
 
+    ob_start();
+
     get_template_part( 'templates/elastic/list', $args['post_type'] );
 
     return ob_get_clean();
