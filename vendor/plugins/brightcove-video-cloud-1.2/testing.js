8b4b65225c4bb974c6af684d43fb1c434fb017c9
Andy Potanin <andy@udx.io>
2014-10-09 07:28:28 +0000

    removed brightcove-video-cloud-1.2 plugin

--- vendor/plugins/brightcove-video-cloud-1.2/testing.js
+++
@@ -1,7 +0,0 @@
-	document.ready.onLoad()
-	{
-		nc = document.getElementById("content");
-   	nc.addEventListener("blur", function(e) {console.log("select starts at ", this.selectStart)}, false);
-	}
-
-	
