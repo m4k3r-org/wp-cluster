5484f785d134363603aedd424978594826f427e2
Reid Williams <reid.williams@usabilitydynamics.com>
2014-10-14 10:47:04 -0400

    Squashed 'vendor/themes/wp-spectacle-2/' content from commit 0ee9142
    git-subtree-dir: vendor/themes/wp-spectacle-2
    git-subtree-split: 0ee9142cabfe2ee3214014646f0cad84f998d9c8

---
+++ footer.php
@@ -0,0 +1,24 @@
+</div> <!-- #doc -->
+
+<?php if ( has_nav_menu( 'main-navigation' ) ): ?>
+
+<div class="navigation-overlay overlay">
+
+  <a href="#" class="overlay-close">
+    <span class="icon-spectacle-close"></span>
+    Close
+  </a>
+  <div class="overlay-content">
+    <nav class="clearfix">
+      <?php
+        $menu = new Spectacle_Navigation_Builder();
+        echo $menu->get( 'main-navigation' );
+      ?>
+    </nav>
+  </div>
+</div>
+
+<?php endif; ?>
+<?php wp_footer(); ?>
+</body>
+</html>
