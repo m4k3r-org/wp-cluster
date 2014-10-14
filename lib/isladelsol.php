130e0cb0ad6f969fa60742761cef4712e3c3f884
Reid Williams <reid.williams@usabilitydynamics.com>
2014-10-14 10:47:05 -0400

    Squashed 'vendor/themes/wp-spectacle-isladelsol/' content from commit a5d8194
    git-subtree-dir: vendor/themes/wp-spectacle-isladelsol
    git-subtree-split: a5d819497f88d2aa0a531bd42925cf583f6c5eeb

---
+++ lib/isladelsol.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WP_Spectacle\IslaDelSol;
+
+use WP_Spectacle\Core as ParentCore;
+
+
+class Core extends ParentCore
+{
+
+}
---
+++ wp-social-stream/static/templates/social_item_list.php
@@ -0,0 +1,3 @@
+<script type="text/html" id="wp_social_stream_social_item_list">
+  <div class="row masonry-container" data-bind="template: { name: 'wp_social_stream_social_item_single', foreach: $data, as: 'result', afterRender: window.scmfStream.checkState }"></div>
+</script>
