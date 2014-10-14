667d05e9e14f8ba14dc4fbdb1166865cad67642a
Reid Williams <reid.williams@usabilitydynamics.com>
2014-10-14 03:22:09 -0400

    * Removing wp-spectacles [ci skip]

--- vendor/themes/wp-spectacle-fbt/wp-social-stream/static/templates/social_item_single.php
+++
@@ -1,3 +0,0 @@
-<script type="text/html" id="wp_social_stream_social_item_single">
-  <a data-bind="template: { name: 'wp_social_stream_social_item_single_' + type(), data: $data }, attr: { 'href': url, 'class': 'item item-' + type() }"></a>
-</script>
