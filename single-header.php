ef0324b9ce7df083e5cb7ff1b92f3ce18c7b011f
George Fekete <george@primalskill.com>
2014-10-15 20:20:10 +0300

    Fix for blog single header

--- single-header.php
+++ single-header.php
@@ -11,9 +11,9 @@ if ( has_post_thumbnail() )
 <header class="single-post-header" style="background-image: url('<?php echo $image_url; ?>');">
   <div class="gradient-fade"></div>
 
-  <section class="presenter-logos">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/presenter-logos.png" alt="Disco Donnie Presents ultimo">
-  </section>
+  <a href="/">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/logo.png" alt="Monster Block Party" style="width: 210px; padding-top: 20px;">
+  </a>
 
   <?php
 
