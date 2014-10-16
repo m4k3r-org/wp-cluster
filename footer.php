355ae80013f795f8573c55da154b09f63348c163
attilaf <attilaf@primalskill.com>
2014-10-16 16:23:14 +0300

    Fixed menu overlay.

--- footer.php
+++ footer.php
@@ -33,27 +33,27 @@ $menu = new Spectacle_Navigation_Builder();
       </div>
     <?php endif; ?>
 
-
-    <?php if( has_nav_menu( 'main-navigation' ) ): ?>
-
-      <div class="navigation-overlay overlay">
-
-        <a href="#" class="overlay-close">
-          <span class="icon-spectacle-close"></span>
-          Close
-        </a>
-
-        <div class="overlay-content">
-          <nav class="clearfix">
-            <?php
-            echo $menu->get( 'main-navigation' );
-            ?>
-          </nav>
-        </div>
-      </div>
-
-    <?php endif; ?>
   </footer>
+<?php endif; ?>
+
+<?php if( has_nav_menu( 'main-navigation' ) ): ?>
+
+  <div class="navigation-overlay overlay">
+
+    <a href="#" class="overlay-close">
+      <span class="icon-spectacle-close"></span>
+      Close
+    </a>
+
+    <div class="overlay-content">
+      <nav class="clearfix">
+        <?php
+        echo $menu->get( 'main-navigation' );
+        ?>
+      </nav>
+    </div>
+  </div>
+
 <?php endif; ?>
 
 <?php
