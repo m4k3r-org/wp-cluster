c3de7b6c04bf7715a0069704cbad55fe330f3138
Reid Williams <reid.williams@usabilitydynamics.com>
2014-10-16 01:38:15 -0400

    * PHP 5.3 compat

--- lib/twitter.php
+++ lib/twitter.php
@@ -279,8 +279,10 @@ class TwitterOAuth {
 
 /* Generic exception class
  */
-class OAuthException extends Exception {
-  // pass
+if( !class_exists( 'OAuthException' ) ){
+  class OAuthException extends Exception {
+    // pass
+  }
 }
 
 class OAuthConsumer {
