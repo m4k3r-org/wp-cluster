51f0d25e56cba0a2318613c9efde211d9183c981
Reid Williams <reid.williams@usabilitydynamics.com>
2014-10-22 13:15:14 -0400

    Squashed 'vendor/themes/wp-spectacle-isladelsol/' changes from ba36578..ee5549c
    ee5549c Artist lineup widget icon margin
    d9aebbd Artist lineup widget improvements
    
    git-subtree-dir: vendor/themes/wp-spectacle-isladelsol
    git-subtree-split: ee5549cd91b228a85592fe3d5ac0707d438e99ca

--- page-home.php
+++ page-home.php
@@ -7,95 +7,95 @@ get_header();
 the_post();
 
 ob_start();
-dynamic_sidebar('header_widget_area');
+dynamic_sidebar( 'header_widget_area' );
 $artist_lineup = ob_get_clean();
 
-$artist_lineup = str_replace('}{', '},{', $artist_lineup);
-$artist_lineup = '[' . $artist_lineup .']';
+$artist_lineup = str_replace( '}{', '},{', $artist_lineup );
+$artist_lineup = '[' . $artist_lineup . ']';
 $artist_lineup = json_decode( $artist_lineup, true );
 
 ?>
 
-  <header>
-    <section class="presenter-logos">
-      <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/presenter-logos.png" alt="Disco Donnie Presents GlobalGroove London">
-    </section>
+<header>
+  <section class="presenter-logos">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/presenter-logos.png" alt="Disco Donnie Presents GlobalGroove London">
+  </section>
 
-    <h1 class="main-logo">
-      <a href="/">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/main-logo-big.png" alt="Isla Del Sol">
-      </a>
-    </h1>
-  </header>
+  <h1 class="main-logo">
+    <a href="/">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/static/images/main-logo-big.png" alt="Isla Del Sol">
+    </a>
+  </h1>
+</header>
 
-  <?php if ( isset( $artist_lineup[0] ) ): ?>
+<?php if( isset( $artist_lineup[ 0 ] ) ): ?>
 
   <div class="diamond-box diamond-box-left">
     <div class="inner">
       <strong>
-        <?php echo date( 'l', strtotime( $artist_lineup[0][ 'data' ][ 'date' ] ) ); ?>
-        <br><?php echo date( 'F jS', strtotime( $artist_lineup[0][ 'data' ][ 'date' ] ) ); ?>
+        <?php echo $artist_lineup[ 0 ][ 'data' ][ 'text1' ]; ?>
+        <br><?php echo $artist_lineup[ 0 ][ 'data' ][ 'text2' ]; ?>
       </strong>
 
-      <?php echo $artist_lineup[0][ 'data' ][ 'location' ]; ?>
+      <?php echo $artist_lineup[ 0 ][ 'data' ][ 'text3' ]; ?>
 
       <hr>
-      <img src="<?php echo $artist_lineup[0][ 'data' ]['image_source'] ?>" alt="Day 1">
+      <img src="<?php echo $artist_lineup[ 0 ][ 'data' ][ 'image_source' ] ?>" alt="Day 1">
     </div>
   </div>
 
-  <?php endif; ?>
+<?php endif; ?>
 
-  <?php if ( isset( $artist_lineup[1] ) ): ?>
+<?php if( isset( $artist_lineup[ 1 ] ) ): ?>
 
   <div class="diamond-box diamond-box-right">
     <div class="inner">
       <strong>
-        <?php echo date( 'l', strtotime( $artist_lineup[1][ 'data' ][ 'date' ] ) ); ?>
-        <br><?php echo date( 'F jS', strtotime( $artist_lineup[1][ 'data' ][ 'date' ] ) ); ?>
+        <?php echo $artist_lineup[ 1 ][ 'data' ][ 'text1' ]; ?>
+        <br><?php echo $artist_lineup[ 1 ][ 'data' ][ 'text2' ]; ?>
       </strong>
-      <?php echo $artist_lineup[1][ 'data' ][ 'location' ]; ?>
+      <?php echo $artist_lineup[ 1 ][ 'data' ][ 'text3' ]; ?>
 
       <hr>
       <span>Exlusive VIP Pool Party</span>
 
-      <img src="<?php echo $artist_lineup[1][ 'data' ]['image_source'] ?>" alt="Day 2">
+      <img src="<?php echo $artist_lineup[ 1 ][ 'data' ][ 'image_source' ] ?>" alt="Day 2">
 
       <small>Plus Over 20 Regional Artists</small>
     </div>
   </div>
 
-  <?php endif; ?>
+<?php endif; ?>
 
-  <div class="clearfix"></div>
-  <div class="content-container clearfix">
-    <div class="triangle-top"></div>
-    <div class="content-inner">
+<div class="clearfix"></div>
+<div class="content-container clearfix">
+  <div class="triangle-top"></div>
+  <div class="content-inner">
 
-      <a href="http://www.eventbrite.com/e/isla-del-sol-2014-tickets-12353536761?aff=spectacle" onclick="javascript:_gaq.push( [ '_link', 'http://www.eventbrite.com/e/isla-del-sol-2014-tickets-12353536761?aff=spectacle' ] ); return false;" class="buy-tickets" target="_blank">
-        <div class="inner">
-          Buy
-          <strong>Tickets</strong>
-        </div>
-      </a>
-
-      <h2>Join Us</h2>
-
-      <p class="main-content">The summer season may be coming to a close, but we'll be going out with a bang. This Labor Day weekend, Isla Del Sol Fest is returning to South Padre Island and for the third edition, we’re taking over two new locations – Schlitterbahn Beach Waterpark on Saturday, August 30th and Peninsula Island Resort & Spa on Sunday, August 31st.</p>
-
-      <h1>#isla2014</h1>
-      <hr>
-
-      <div class="stream">
-        <div class="container">
-          <?php the_content(); ?>
-        </div>
+    <a href="http://www.eventbrite.com/e/isla-del-sol-2014-tickets-12353536761?aff=spectacle" onclick="javascript:_gaq.push( [ '_link', 'http://www.eventbrite.com/e/isla-del-sol-2014-tickets-12353536761?aff=spectacle' ] ); return false;" class="buy-tickets" target="_blank">
+      <div class="inner">
+        Buy
+        <strong>Tickets</strong>
       </div>
+    </a>
 
+    <h2>Join Us</h2>
+
+    <p class="main-content">The summer season may be coming to a close, but we'll be going out with a bang. This Labor Day weekend, Isla Del Sol Fest is returning to South Padre Island and for the third edition, we’re taking over two new locations – Schlitterbahn Beach Waterpark on Saturday, August 30th and Peninsula Island Resort & Spa on Sunday, August 31st.</p>
+
+    <h1>#isla2014</h1>
+    <hr>
+
+    <div class="stream">
+      <div class="container">
+        <?php the_content(); ?>
+      </div>
     </div>
-  </div>
 
-<?php get_template_part('page-home', 'contest'); ?>
+  </div>
+</div>
+
+<?php get_template_part( 'page-home', 'contest' ); ?>
 
 
 <?php get_footer(); ?>
