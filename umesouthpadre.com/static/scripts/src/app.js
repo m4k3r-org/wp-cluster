e2243992279878189165901a25739c9117769986
George Fekete <george@primalskill.com>
2014-10-22 12:50:27 +0300

    Adding video popup in the header

--- umesouthpadre.com/static/scripts/src/app.js
+++ umesouthpadre.com/static/scripts/src/app.js
@@ -9,16 +9,47 @@ require.config({
 	baseUrl: "static/scripts/src",
 	paths: {
 		"jquery": "vendor/jquery/jquery"
+	},
+	shim: {
+		"vendor/fancybox-2.1.5/jquery.fancybox": ["jquery"],
+		"vendor/fancybox-2.1.5/helpers/jquery.fancybox-media": ["jquery", "vendor/fancybox-2.1.5/jquery.fancybox"]
 	}
 });
 
 
 require( [
 	'lib/share',
-	'lib/countdown'
-], function( share, countdown ){
+	'lib/countdown',
+	'lib/istouch',
+	'vendor/fancybox-2.1.5/jquery.fancybox',
+	'vendor/fancybox-2.1.5/helpers/jquery.fancybox-media'
+], function( share, countdown, isTouch ){
 
 	share.init();
 	countdown.init();
+
+	// Initialize top video popup
+	if ( isTouch === false )
+	{
+		$('header .video-popup' ).fancybox({
+			maxWidth: 800,
+			maxHeight: 600,
+			fitToView: false,
+			autoSize: false,
+			closeClick: false,
+			padding: 0,
+			margin: 0,
+
+			helpers: {
+				media: true
+			},
+
+			youtube: {
+				autoplay: 1,
+				hd: 1
+			}
+		});
+	}
+
 
 } );
