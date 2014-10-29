2b72ec7cf3e1f0c1dc5a9b5f34344c6f98dfb860
andypotanin <andy.potanin@usabilitydynamics.com>
2014-10-29 05:04:51 -0400

    Squashed 'vendor/plugins/wp-event-post-type/' content from commit c23ebb4
    git-subtree-dir: vendor/plugins/wp-event-post-type
    git-subtree-split: c23ebb4f397b75f032c8eb452d73b0ad762b43d7

---
+++ wp-event-post-type.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name: Post Type: Event
+ * Plugin URI: http://usabilitydynamics.com/
+ * Description: Application managing must-use plugins and obfuscation rewrites.
+ * Version: 0.5.0
+ * Author: Usability Dynamics
+ * Author URI: http://usabilitydynamics.com/
+ * License: GPLv2 or later
+ * Network: False
+ * GitHub Plugin URI: wpCloud/wp-event-post-type
+ *
+ */
+
+
