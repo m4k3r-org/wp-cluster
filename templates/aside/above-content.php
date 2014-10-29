a4681f964afe2bc4163437fca13a11151c75e1fe
andypotanin <andy.potanin@usabilitydynamics.com>
2014-10-29 05:01:38 -0400

    Squashed 'vendor/themes/wp-festival-2/' content from commit b0cff23
    git-subtree-dir: vendor/themes/wp-festival-2
    git-subtree-split: b0cff23140990d705277aa021255d284687465b5

---
+++ templates/aside/above-content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ *
+ * Default "above-content" aside
+ *
+ */
+
---
+++ wp-social-stream/static/templates/social_item_list.php
@@ -0,0 +1,4 @@
+<script type="text/html" id="wp_social_stream_social_item_list">
+  <section class="info-stream clearfix" data-bind="template: { name: 'wp_social_stream_social_item_single', foreach: $data, as: 'result', afterRender: window.scmfStream.checkState }">
+  </section>
+</script>
