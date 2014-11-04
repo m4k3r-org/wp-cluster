eb98d4ee20a3483e893efddd0b36cbd70d926a05
George Fekete <george@primalskill.com>
2014-11-04 19:34:52 +0200

    Bug fixed in wp-disco version 2

--- footer.php
+++ footer.php
@@ -57,7 +57,7 @@
           <div class="cfct-module">
             <?php wp_nav_menu( array( 'theme_location' => 'bottom_of_page_menu', 'menu_class' => 'footer-nav flawless-menu', 'fallback_cb' => 'flawless_list_pages' ) ); ?>
             <div class="footer_copyright">Copyright 1993-<?php echo date( 'Y' ); ?> SFX-Disco Operating LLC</div>
-            <div class="footer_copyright">v2.2.0</div>
+            <div class="footer_copyright">v2.2.1</div>
           </div>
         </div>
 
