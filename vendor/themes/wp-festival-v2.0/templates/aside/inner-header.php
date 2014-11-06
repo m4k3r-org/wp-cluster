e8a05b818275c6ffa3a2eed7e4182d37d039ab13
George Fekete <george@primalskill.com>
2014-11-06 15:43:25 +0200

    temporary, adding subtree... [ci skip]

--- vendor/themes/wp-festival-v2.0/templates/aside/inner-header.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The Header Aside on inner pages
- *
- * @author Usability Dynamics
- * @module wp-festival
- * @since wp-festival 2.0.0
- */
-?>
--- vendor/themes/wp-festival-v2.0/wp-social-stream/static/templates/social_item_single_home.php
+++
@@ -1,3 +0,0 @@
-<script type="text/html" id="wp_social_stream_social_item_single_home">
-  <div data-bind="template: { name: 'wp_social_stream_social_item_single_' + type(), data: $data }, attr: { 'href': url, 'class': type() + '-streams single-item' }"></div>
-</script>
