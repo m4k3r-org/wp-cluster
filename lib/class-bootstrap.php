031a95813546ac4bd4955a987f1b81564ff30eaa
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-08 15:06:23 -0500

    Squashed 'vendor/plugins/wp-veneer/' changes from 8ef402a..3d7c436
    3d7c436 updated wp-spectable-v2.0 via composer
    e8e4b80 wip on wp-veneer config loading improvements
    e29c1c4 added support for nested settings via composer
    a21df14 [ci skip]
    b493fed wip
    448c57c improving toolbar info
    394bf67 removed w3 total cache because it was failing on admin, need to test properly locally first
    1c9b0a7 * Fixing rules [ci skip]
    
    git-subtree-dir: vendor/plugins/wp-veneer
    git-subtree-split: 3d7c436ac82621d85e0f0fcc68d1499d1228a87c

--- lib/class-bootstrap.php
+++ lib/class-bootstrap.php
@@ -41,7 +41,7 @@ namespace UsabilityDynamics\Veneer {
        * @property $version
        * @type {Object}
        */
-      public static $version = '0.6.1';
+      public static $version = '0.7.0';
 
       /**
        * Textdomain String
@@ -240,7 +240,7 @@ namespace UsabilityDynamics\Veneer {
 	      /** Initialize Components. */
         $this->_components();
 
-        add_action( 'init', array( $this, 'init' ) );
+        add_action( 'setup_theme', array( $this, 'setup_theme' ) );
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
         add_action( 'wp_head', array( $this, 'wp_head' ), 0, 200 );
@@ -292,18 +292,6 @@ namespace UsabilityDynamics\Veneer {
             $this->set( 'styles.available', false );
           }
 
-        }
-
-        if( is_dir( ABSPATH . 'wp-content/themes' ) ) {
-          register_theme_directory( ABSPATH . 'wp-content/themes' );
-        }
-
-        if( defined( 'WP_THEME_DIR' ) && is_dir( WP_THEME_DIR ) ) {
-          register_theme_directory( WP_THEME_DIR );
-        }
-
-        if( defined( 'WP_VENEER_THEME_DIR' ) && is_dir( WP_VENEER_THEME_DIR ) ) {
-          register_theme_directory( WP_VENEER_THEME_DIR );
         }
 
       }
@@ -575,11 +563,34 @@ namespace UsabilityDynamics\Veneer {
       }
 
 	    /**
+	     * Ran before theme is setup.
 	     *
+	     * This is the last action before wp_templating_constants() method is called in wp-settings.php which sets the following consants:
+	     * - TEMPLATEPATH
+	     * - STYLESHEETPATH
+	     * - WP_DEFAULT_THEME (if not set)
+	     *
+	     * The theme's functions.php is loaded next, followed by "after_setup_theme" action.
+	     *
+	     * @method setup_theme
 	     */
-      public function init() {
+      public function setup_theme() {
+	      global $wp_theme_directories;
 
-        // Only admin can see W3TC notices and errors
+	      if( defined( 'WP_THEME_DIR' ) && is_dir( WP_THEME_DIR ) ) {
+		      //register_theme_directory( WP_THEME_DIR );
+	      }
+
+	      if( defined( 'WP_VENEER_THEME_DIR' ) && is_dir( WP_VENEER_THEME_DIR ) ) {
+		      //register_theme_directory( WP_VENEER_THEME_DIR );
+	      }
+
+	      // Only register default theme directory if no other directories are registerd
+	      if( !$wp_theme_directories || (is_array( $wp_theme_directories ) && empty( $wp_theme_directories ) ) && is_dir( ABSPATH . 'wp-content/themes' ) ) {
+		      register_theme_directory( ABSPATH . 'wp-content/themes' );
+	      }
+
+	      // Only admin can see W3TC notices and errors
         // add_action('admin_notices', array( $this, 'admin_notices' ));
         // add_action('network_admin_notices', array( $this, 'admin_notices' ));
 
@@ -793,6 +804,8 @@ namespace UsabilityDynamics\Veneer {
 
       /**
        * Administrative Toolbar.
+       *
+       * @author potanin@UD
        */
       public function toolbar_local(){
         global $wp_admin_bar;
@@ -807,34 +820,46 @@ namespace UsabilityDynamics\Veneer {
           $_environment = PHP_ENV;
         }
 
-        if( current_user_can( 'manage_options' ) ){
+	      $_ip = $_SERVER[ 'SERVER_ADDR' ];
+
+	      if( $_ip === '::1' ) {
+		      $_ip = 'localhost';
+	      }
+
+        if( current_user_can( 'manage_options' ) ) {
           /** Add the style we need */ ?>
-          <style>
-            #wp-admin-bar-server_name > a:before {
-              content: "\f177";
-              top: 2px;
-            }
-          </style> <?php 
-          /** Add the menu items */
+          <style> #wp-admin-bar-server_name > a:before { content: "\f177"; top: 2px; }</style> <?php
+
           $wp_admin_bar->add_menu( array(
             'id' => 'server_name',
             'parent' => 'top-secondary',
-            'title' => gethostname(),
+            'title' => sprintf( __( 'Host: %s' ), gethostname() ),
             'href' => '#'
-          ) );
+          ));
+
           $wp_admin_bar->add_menu( array(
             'id' => 'environment',
             'parent' => 'server_name',
-            'title' => sprintf( __( '[%s]' ), $_environment ),
+            'title' => sprintf( __( 'Branch: %s' ), $_environment ),
             'href' => '#'
-          ) );
+          ));
+
+          $wp_admin_bar->add_menu( array(
+            'id' => 'db_host',
+            'parent' => 'server_name',
+            'title' => sprintf( __( 'DB: %s/%s' ), DB_HOST, DB_NAME ),
+            'href' => '#'
+          ));
+
           $wp_admin_bar->add_menu( array(
             'id' => 'ip_address',
             'parent' => 'server_name',
-            'title' => $_SERVER[ 'SERVER_ADDR' ],
+            'title' => sprintf( __( 'IP: %s' ), $_ip ),
             'href' => '#'
           ) );
+
         }
+
       }
 
       /**
--- wp-veneer.php
+++ wp-veneer.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP-Veneer
  * Plugin URI: http://usabilitydynamics.com/
  * Description: Veneer.io Helper.
- * Version: 0.6.9
+ * Version: 0.7.0
  * Author: Usability Dynamics
  * Author URI: http://usabilitydynamics.com/
  * License: GPLv2 or later
