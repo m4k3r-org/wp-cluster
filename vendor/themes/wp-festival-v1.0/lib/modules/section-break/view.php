4c770c5f3a881337295a28a7e95af205639d4ea0
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-08 18:06:54 -0500

    removing wp-festival v1.0 to be added as subtree

--- vendor/themes/wp-festival-v1.0/lib/modules/section-break/view.php
+++
@@ -1,3 +0,0 @@
-<!-- Section Break Module -->
-<div class="section-break clear <?php echo !empty($data[$this->get_field_name( 'type' )])?$data[$this->get_field_name( 'type' )]:''; ?>"><!-- --></div>
-<!-- #Section Break Module -->
