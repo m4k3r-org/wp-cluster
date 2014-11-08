db922d61ab3609f2b5b8348efa4198674e1f352a
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-08 09:22:40 -0500

    removing wp-social-stream, to be added as subtree

--- vendor/plugins/wp-social-stream/static/templates/js.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" language="javascript">
-  /** Setup our variable */
-  wp_social_stream = {
-    base_url: "<?php echo addcslashes( plugins_url( '/wp-social-stream/static/scripts/' ), '"' ); ?>"
-  };
-</script>
--- vendor/plugins/wp-social-stream/static/templates/social_item_single.php
+++
@@ -1,5 +0,0 @@
-<script type="text/html" id="wp_social_stream_social_item_single">
-  <li data-bind="attr: { 'id': id, 'rel': rel, 'data-net': type, 'class': 'dcsns-li dcsns-' + type(), 'url': url }">
-    <div data-bind="template: { name: 'wp_social_stream_social_item_single_' + type(), data: $data }"></div>
-  </li>
-</script>
