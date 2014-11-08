eb591086a5c0e05f6543f4e6eb74a11474dd476d
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-08 09:28:35 -0500

    removing wp-vertical-edm plugin, readding as subtree

--- vendor/plugins/wp-vertical-edm/static/scripts/src/intelligence.js
+++
@@ -1,7 +0,0 @@
-define( 'intelligence-app', [ 'analysis.visualizer', 'analysis.client' ], function() {
-  console.debug( 'intelligence-app', 'loaded' );
-
-  var client      = require( 'analysis.client' );
-  var visualizer  = require( 'analysis.visualizer' );
-
-});
