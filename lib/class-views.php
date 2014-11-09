e5f0ac3902eb508d6633824a14ed4cb1165f15b7
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-08 20:18:30 -0500

    Squashed 'wp-content/plugins/wp-cluster/' content from commit a0e960e
    git-subtree-dir: wp-content/plugins/wp-cluster
    git-subtree-split: a0e960eddc7eaeec96f2fa7b5c6381aafd5e65f5

---
+++ lib/class-views.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Views Access Controller
+ *
+ * @module Cluster
+ * @author potanin@UD
+ */
+namespace UsabilityDynamics\Cluster {
+
+  if( !class_exists( 'UsabilityDynamics\Cluster\Views' ) ) {
+
+    /**
+     * Class Views
+     *
+     * @module Cluster
+     */
+    class Views {
+
+      /**
+       * Initialize Views
+       *
+       * @for Views
+       */
+      public function __construct() {
+
+      }
+
+    }
+
+  }
+
+}
---
+++ static/views/settings-site.php
@@ -0,0 +1,5 @@
+<div class="wrap">
+  <h2 class="title">Veneer Settings</h2>
+  <p>Veneer settings for single sites.</p>
+
+</div>
