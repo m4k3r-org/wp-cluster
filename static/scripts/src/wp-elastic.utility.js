6d8d596fb7e009d27715ef2ce1c8db9bcff79376
andypotanin <andy.potanin@usabilitydynamics.com>
2014-11-09 05:49:47 -0500

    Squashed 'wp-content/plugins/wp-elastic/' content from commit 1f7e69c
    git-subtree-dir: wp-content/plugins/wp-elastic
    git-subtree-split: 1f7e69ce43a13d37475dc3f953c83a48ce1af71a

---
+++ static/scripts/src/wp-elastic.utility.js
@@ -0,0 +1,12 @@
+/**
+ * wpElastic Utility.
+ *
+ * @module wp-elastic
+ * @author potanin@UD
+ */
+define( 'wp-elastic.utility', function wpElasticUtility() {
+  console.debug( 'wp-elastic.utility' );
+
+  return {};
+
+});
