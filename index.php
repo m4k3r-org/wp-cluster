1022c1614d9db9fbcd59021faaec8e3a57845cb0
andypotanin <andy@udx.io>
2014-11-21 15:40:23 -0500

    updated to wp 4.0.1

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
 <?php
 /**
- * WordPress Site Loader
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
  */
-try {
 
-  // header( 'PageSpeed: off' );
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-  /** Make sure we have a proper wp-config file */
-  if( !file_exists( __DIR__ . '/wp-config.php' ) ) {
-    throw new Exception( 'Site not installed.' );
-  }
-
-  /** Make sure we have our vendor libraries installed, and if we - include them */
-  if( !file_exists( __DIR__ . '/wp-blog-header.php' ) ) {
-    throw new Exception( 'Site vendor libraries not installed.' );
-  }else {
-    require_once( __DIR__ . '/wp-blog-header.php' );
-  }
-
-} catch( Exception $e ) {
-
-  /** There was an issue, we need to bail */
-  header( 'HTTP/1.1 500 Internal Server Error' );
-  echo '<h1>Site Error</h1>';
-  echo '<p>' . $e->getMessage() . '</p>';
-  die();
-
-}
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
