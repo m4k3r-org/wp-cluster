31445448712b2d3528d598280c2ab3cdba83ef95
andypotanin <andy@udx.io>
2014-12-07 03:02:44 -0500

    manually added develop-branch changes into plugins,themes,vendor, etc.

---
+++ wp-cluster.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: WP-Cluster
+ * Plugin URI: http://usabilitydynamics.com/
+ * Description: Application managing must-use plugins and obfuscation rewrites.
+ * Version: 0.4.4
+ * Author: Usability Dynamics
+ * Author URI: http://usabilitydynamics.com/
+ * License: GPLv2 or later
+ * Network: True
+ * Domain Path: /static/locale/
+ * Text Domain: wp-cluster
+ * GitHub Plugin URI: UsabilityDynamics/wp-cluster
+ *
+ * The Loder class is self-initializing.
+ *
+ * @example
+ *
+ *      UsabilityDynamics\Cluster::get_instance()->state->settings;
+ *      UsabilityDynamics\Cluster::get_instance()->get()
+ *
+ * @namespace Cluster
+ * @module Cluster
+ */
+
+if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	include_once( __DIR__ . '/vendor/autoload.php' );
+}
+
+// Include bootstrap.
+if( !class_exists( 'UsabilityDynamics\Cluster\Bootstrap' ) ) {
+	include_once( __DIR__ . '/lib/class-bootstrap.php' );
+}
+
+// Initialize.
+if( class_exists( 'UsabilityDynamics\Cluster\Bootstrap' ) ) {
+	new UsabilityDynamics\Cluster\Bootstrap();
+}
